a66add12c0cfb605ddf09b29b5cbe6b05ff5235a
OchianIulian <ochian_a@yahoo.ro>
2023-12-06 16:28:34 -0500

    Add first client server conection

---
+++ src/donator.c
@@ -0,0 +1,66 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <string.h>
+
+/*codul de eroare returnat de anumite apeluri*/
+extern int errno;
+
+/*portul*/
+int port;
+
+int main(int argc, char *argv[]){
+    int sd;//descriptorul de socket
+    struct sockaddr_in server;//structura pentru server
+    char msg[100];//mesajul de trimis
+
+    /*verificam daca exista 3 argumente in apelare*/
+    if(argc!=3){
+        printf("Sintaxa: %s <adresa_server> <port>\n", argv[0]);
+        return errno;
+    }
+
+    /*stabilim portul*/
+    port = atoi(argv[2]);
+
+    /*ultimul parametru e 0, ca valoare pentru protocol, sa fie ales automat de sistem*/
+    if((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("eroare la crearea socketului");
+        return errno;
+    }
+
+    /*umplem structura folosita pentru realizarea conexiunii cu serverul*/
+    /*familia socketului*/
+    server.sin_family = AF_INET;
+    /*portul*/
+    server.sin_port = htons(port);
+    /*adresa IP a serverului*/
+    server.sin_addr.s_addr = inet_addr(argv[1]);
+
+    /*ne conectam la server*/
+    if(connect(sd, (struct sockaddr*)&server, sizeof(struct sockaddr)) == -1){
+        perror("[client]Eroare la connect\n");
+        return errno;
+    }
+
+    /*citirea mesajului*/
+    bzero(msg, 100);
+    printf("[client]Introduceti un nume:  ");
+    fflush(stdout);
+    read(0, msg, 100);
+
+    /*trimitem mesajul la server*/
+    //ultimul paramentru e setat pe 0 pentru operatia de trimitere standard
+    if(send(sd, msg, 100, 0) <=0){
+        perror("[client] Eroare la write\n");
+        return errno;
+    }
+
+    close(sd);
+
+}
---
+++ src/server.c
@@ -0,0 +1,97 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*Portul folosit*/
+#define PORT 2024
+
+/*codul de eroare returnat de anumite apeluri*/
+extern int errno;
+
+int main(){
+    struct sockaddr_in server;/*structura folosita de server*/
+    struct sockaddr_in from;/*structura clientului*/
+    char msg[100];/*mesajul primit de la client*/
+    char msgrasp[100]="";/*mesaj de raspuns pentru client*/
+    int sd;/*socket descriptor*/
+
+    /*crearea unui socket*/
+    if((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("Eroare la crearea socketului\n");
+        return errno;
+    }
+
+    /*pregatirea structurilor de date si initializarea lor cu 0*/
+    bzero(&server, sizeof(server));
+    bzero(&from, sizeof(from));
+
+    /*umplem structura folosita de server*/
+    /*stabilirea tipului de adresa a socketului/familiei socketului*/
+    server.sin_family = AF_INET;
+    /*acceptam orice adresa  si asiguram compatibilitatea pe orice arhitectura*/
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    /*setam portul*/
+    server.sin_port = htons(PORT);
+
+    /*se atașează un socketul la adresa IP și portul specific*/
+    if(bind(sd, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1){
+        perror("[server]eroare la bind()\n");
+        printf("Error code: %d\n", errno); 
+        return errno;
+    }
+
+    /*punem serverul sa asculte daca vin clienti sa se conecteze, maxim 5 odata*/
+    if(listen(sd, 5) == -1){
+        perror("[server]Eroare la listen\n");
+        return errno;
+    }
+
+    /*servim in mod iterativ clientii*/
+    while (1)
+    {
+        int client;
+        int length = sizeof(from);
+
+        printf("[server]Asteptam la portul %d...", PORT);
+        fflush(stdout);
+
+        /*acceptam un client ()*/
+        client = accept(sd, (struct sockaddr*)&from, &length);
+
+        /*eroare la acceptarea unui client*/
+        if(client<0){
+            perror("[server]Eroare la accept()");
+            fflush(stdout);
+            continue;
+        }
+
+        /*s-a realizat conexiunea, se asteapta mesajul*/
+        /*ne asiguram ca bufferul nu contine nimic*/
+        bzero(msg, 100);
+        printf("[server]asteptam mesajul...");
+        fflush(stdout);
+
+        /*citirea citim mesajul*/
+        if(recv(client, msg, 100, 0)<=0){//ultimul parametru este setat pe 0 pentru o operațiune de recepție standard.
+            perror("[server]Eroare la read() de la client");
+            fflush(stdout);
+            continue;
+        }
+
+        printf("Mesajul a fost receptionat...%s\n", msg);
+
+
+
+
+        close(client);
+    }
+    
+
+
+    return 0;
+}
