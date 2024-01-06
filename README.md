1aac3cd55a0d7572ccf7a17119490796d6814297
OchianIulian <ochian_a@yahoo.ro>
2024-01-06 10:41:45 -0500

    feat: Add functions for connect and insert or update data into database but there are some errors that must be resolved

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FoodWasteReducer
+
+# Compilare server
+gcc server.c map.c -o server -lsqlite3
+
+# Rulare executabil
+./server
--- src/server.c
+++ src/server.c
@@ -30,6 +30,104 @@ typedef struct {
 /*depozitul de alimente*/
 Map depozit;
 
+/*database transactions*/
+/*Create connection to database*/
+int connect_to_database(sqlite3 *db){
+    int rc;//return code
+    rc = sqlite3_open("depozit.db", &db);
+	
+	if(rc != SQLITE_OK){
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      		return(-1);
+	} else {
+		fprintf(stderr, "Opened database successfully\n");
+	}
+
+    char *sql = "CREATE TABLE IF NOT EXISTS MapData (Key TEXT PRIMARY KEY NOT NULL, Value INT NOT NULL);";
+
+    rc = sqlite3_exec(db, sql, 0, 0, 0);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Eroare la crearea tabelului: %s\n", sqlite3_errmsg(db));
+    } else {
+        fprintf(stdout, "Tabelul 'MapData' a fost creat cu succes sau exista deja!\n");
+    }
+	return rc;
+}
+
+
+void insertOrUpdateData(sqlite3 *db, const char *key, int value){
+
+    if (db == NULL || key == NULL) {
+        fprintf(stderr, "Invalid parameters or NULL database connection.\n");
+        return;
+    }
+
+    char sql[100];//comanda sql 
+    sqlite3_stmt *stmt;//e folosit pentru a executa o interogare
+    int rc;
+
+    //verificam daca exista deja o intrare cu aceeasi cheie
+    sprintf(sql, "SELECT Value FROM MapData WHERE Key = '%s';", key);
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);//asociaza statementului interogarea
+    if(rc!=SQLITE_OK){
+        fprintf(stderr, "Eroare la pregatirea interogarii SELECT: %s\n", sqlite3_errmsg(db));
+        return;
+    }
+
+    rc = sqlite3_step(stmt);//executa statement ul
+    /*SQLITE_ROW INFORMEAZA CA EXISTA CEL PUTIN O linie de rezultat disponibila pentru prelucrare*/
+    if(rc == SQLITE_ROW){
+        // Cheia există, putem face UPDATE la valoarea asociată
+        sprintf(sql, "UPDATE MapData SET Value = %d WHERE Key = '%s';", value, key);
+    } else {
+        // Cheia nu există, inserăm o nouă pereche key-value
+        sprintf(sql, "INSERT INTO MapData (Key, Value) VALUES ('%s', %d);", key, value);
+    }
+
+    sqlite3_finalize(stmt);//elibereaza resursele 
+
+    // Executăm comanda SQL adecvată (UPDATE sau INSERT)
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    if(rc != SQLITE_OK){
+        fprintf(stderr, "Eroare la pregatirea interogarii UPDATE/INSERT: %s\n", sqlite3_errmsg(db));
+        return;
+    }
+
+    rc = sqlite3_step(stmt);
+    if(rc != SQLITE_DONE){
+        fprintf(stderr, "Eroare la executia interogarii UPDATE/INSERT: %s\n", sqlite3_errmsg(db));
+    }
+    printf("S-a adaugat produsul in baza de date, totul e ok\n");
+    sqlite3_finalize(stmt);
+}
+
+// Functie pentru citirea datelor din baza de date si salvarea lor in structura Map
+void readData(sqlite3 *db){
+    sqlite3_stmt *stmt;
+    const char *sql = "SELECT Key, Value FROM MapData;";
+
+    //Executam interogareaSQL
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) != SQLITE_OK) {
+        fprintf(stderr, "Eroare la pregatirea interogarii: %s\n", sqlite3_errmsg(db));
+        return;
+    }
+
+    // Citim rezultatele interogarii si le salvam in structura Map/depozit
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+        const unsigned char *key = sqlite3_column_text(stmt, 0);
+        int value = sqlite3_column_int(stmt, 1);
+
+        // Salvam datele in structura Map
+        sprintf(depozit.keys[depozit.size], "%s", key);
+        depozit.values[depozit.size] = value;
+        depozit.size++;
+    }
+
+    sqlite3_finalize(stmt);
+}
+
+/*end database config*/
+
 /* functie de convertire a adresei IP a clientului in sir de caractere */
 char * conv_addr (struct sockaddr_in address)
 {
@@ -47,7 +145,7 @@ char * conv_addr (struct sockaddr_in address)
 }
 
 /* realizeaza primirea si retrimiterea unui mesaj unui client */
-int transactions(int client, char *msg)
+int transactions(int client, char *msg, sqlite3 *db)
 {
     Aliment receivedAliment;    
     memset(&receivedAliment, 0, sizeof(Aliment));
@@ -90,21 +188,11 @@ int transactions(int client, char *msg)
 
     printf("Alimentul a fost receptionat...: Nume=%s; Cantitate=%d\n",
     receivedAliment.nume, receivedAliment.cantity);
-  
-    return 1;
-}
 
-int connect_to_database(sqlite3 *db){
-    int rc;//return code
-    rc = sqlite3_open("depozit.db", &db);
-	
-	if(rc){
-		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-      		return(-1);
-	} else {
-		fprintf(stderr, "Opened database successfully\n");
-	}
-	return rc;
+    /*dupa ce s-au facut tranzactiile inseram si in baza de date*/
+    insertOrUpdateData(db,receivedAliment.nume, getValue(&depozit, receivedAliment.nume));
+    printf("S-au scris in baza de date: %s, %d;\n", receivedAliment.nume, getValue(&depozit, receivedAliment.nume));
+    return 1;
 }
 
 int setup_server(){
@@ -164,7 +252,17 @@ int main(){
 
     int nfds;/*numarul maxim de descriptor*/
     int len;/*lungimea structurii sockaddr_in*/
-    
+
+    /*Database*/
+    sqlite3 *db;
+    int rc = connect_to_database(db);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Error connecting to database\n");
+        return -1;
+    }
+
+
+
     sd = setup_server();
     
     bzero(&from, sizeof(from));
@@ -217,7 +315,7 @@ int main(){
         for(fd = 0; fd<=nfds; ++fd){/*parcurgem multimea de descriptori*/
             /*este un socket de citire pregatit?*/
             if(fd != sd && FD_ISSET(fd, &readfds)){
-                if(transactions(fd, msg)>0){
+                if(transactions(fd, msg, db)>0){
                     printf ("[server] S-a deconectat clientul cu descriptorul %d.\n",fd);
 		            fflush (stdout);
 		            close (fd);		/* inchidem conexiunea cu clientul */
@@ -226,6 +324,6 @@ int main(){
             }
         }
     }
-
+    sqlite3_close(db);
     return 0;
 }
